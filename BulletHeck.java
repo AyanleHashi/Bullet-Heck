6ba08148f6803df6c03f91c8a036520807345e59
Ayanle Hashi <ahashi2000@gmail.com>
2017-02-08 21:26:03 -0500

    Update BulletHeck.java

--- BulletHeck.java
+++ BulletHeck.java
@@ -2,30 +2,6 @@ import java.util.*;
 import java.awt.event.KeyEvent;
 import java.awt.Font;
 
-class Player {
-    
-    public static final double[] coordinates = {0,0};
-    public static final double playerVelocity = 0.3;
-    
-    public static void move() {
-        if (StdDraw.isKeyPressed(KeyEvent.VK_RIGHT)) coordinates[0] += playerVelocity;
-        if (StdDraw.isKeyPressed(KeyEvent.VK_LEFT)) coordinates[0] -= playerVelocity;
-        if (StdDraw.isKeyPressed(KeyEvent.VK_UP)) coordinates[1] += playerVelocity;
-        if (StdDraw.isKeyPressed(KeyEvent.VK_DOWN)) coordinates[1] -= playerVelocity;
-        
-        if (coordinates[0] >= 92.5) coordinates[0] = 92.5;
-        if (coordinates[0] <= -92.5) coordinates[0] = -92.5;
-        if (coordinates[1] >= 92.5) coordinates[1] = 92.5;
-        if (coordinates[1] <= -92.5) coordinates[1] = -92.5;
-    }
-    
-    public static void update() {
-        move();
-        StdDraw.setPenColor(StdDraw.RED);
-        StdDraw.filledRectangle(coordinates[0],coordinates[1],2.5,2.5);
-    }
-}
-
 public class BulletHeck {
     
     public static ArrayList<List<Double>> l = new ArrayList<List<Double>>();
