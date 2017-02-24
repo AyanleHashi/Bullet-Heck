e18de68b919ae17875fa32c623f93120a03161c0
Ayanle Hashi <ahashi2000@gmail.com>
2017-02-24 13:13:27 -0500

    Update Shooter.java

--- Shooter.java
+++ Shooter.java
@@ -3,12 +3,13 @@ import java.awt.event.KeyEvent;
 
 class Player {
     
-    public static double[] coordinates = {0,0};
+    public static double xCoord = 0;
+    public static double yCoord = 0;
     public static final double playerVelocity = 0.15;
     public static final double projectileVel = 3;
     public static ArrayList<List<Double>> projectiles = new ArrayList<List<Double>>();
     public static ArrayList<List<Double>> projectilesBase = new ArrayList<List<Double>>();
-    public static final int initialDelay = 200;
+    public static final int initialDelay = 150;
     public static int delay = initialDelay;
     
     public static Player p = new Player();
@@ -16,32 +17,32 @@ class Player {
     public static void createProjectiles() {
         ArrayList<Double> inner = new ArrayList<Double>();
         if (StdDraw.isKeyPressed(KeyEvent.VK_RIGHT) && delay <= 0) {
-            inner.add(coordinates[0]);
-            inner.add(coordinates[1]);
+            inner.add(xCoord);
+            inner.add(yCoord);
             inner.add(0.1);
             inner.add(0.0);
             projectiles.add(inner);
             delay = initialDelay;
         }
         else if (StdDraw.isKeyPressed(KeyEvent.VK_LEFT) && delay <= 0) {
-            inner.add(coordinates[0]);
-            inner.add(coordinates[1]);
+            inner.add(xCoord);
+            inner.add(yCoord);
             inner.add(-0.1);
             inner.add(0.0);
             projectiles.add(inner);
             delay = initialDelay;
         }
         else if (StdDraw.isKeyPressed(KeyEvent.VK_UP) && delay <= 0) {
-            inner.add(coordinates[0]);
-            inner.add(coordinates[1]);
+            inner.add(xCoord);
+            inner.add(yCoord);
             inner.add(0.0);
             inner.add(0.1);
             projectiles.add(inner);
             delay = initialDelay;
         }
         else if (StdDraw.isKeyPressed(KeyEvent.VK_DOWN) && delay <= 0) {
-            inner.add(coordinates[0]);
-            inner.add(coordinates[1]);
+            inner.add(xCoord);
+            inner.add(yCoord);
             inner.add(0.0);
             inner.add(-0.1);
             projectiles.add(inner);
@@ -60,15 +61,15 @@ class Player {
     }
     
     public static void move() {
-        if (StdDraw.isKeyPressed(KeyEvent.VK_D)) coordinates[0] += playerVelocity;
-        if (StdDraw.isKeyPressed(KeyEvent.VK_A)) coordinates[0] -= playerVelocity;
-        if (StdDraw.isKeyPressed(KeyEvent.VK_W)) coordinates[1] += playerVelocity;
-        if (StdDraw.isKeyPressed(KeyEvent.VK_S)) coordinates[1] -= playerVelocity;
+        if (StdDraw.isKeyPressed(KeyEvent.VK_D)) xCoord += playerVelocity;
+        if (StdDraw.isKeyPressed(KeyEvent.VK_A)) xCoord -= playerVelocity;
+        if (StdDraw.isKeyPressed(KeyEvent.VK_W)) yCoord += playerVelocity;
+        if (StdDraw.isKeyPressed(KeyEvent.VK_S)) yCoord -= playerVelocity;
         
-        if (coordinates[0] >= 97.5) coordinates[0] = 97.5;
-        if (coordinates[0] <= -97.5) coordinates[0] = -97.5;
-        if (coordinates[1] >= 97.5) coordinates[1] = 97.5;
-        if (coordinates[1] <= -97.5) coordinates[1] = -97.5;
+        if (xCoord >= 97.5) xCoord = 97.5;
+        if (xCoord <= -97.5) xCoord = -97.5;
+        if (yCoord >= 97.5) yCoord = 97.5;
+        if (yCoord <= -97.5) yCoord = -97.5;
     }
     
     public static void update() {
@@ -76,38 +77,73 @@ class Player {
         drawProjectiles();
         move();
         StdDraw.setPenColor(255,125,125);
-        StdDraw.filledCircle(coordinates[0],coordinates[1],5);
+        StdDraw.filledCircle(xCoord,yCoord,5);
     }
 }
 
 class Enemy {
-    public static ArrayList<List<Double>> enemies = new ArrayList<List<Double>>();
-    //Format: [xPos,yPos,health]
     public static Random random = new Random();
     
-    public static void createEnemy() {
-        
+    public double xPos;
+    public double yPos;
+    public double vel = 0.05;
+    
+    public Enemy() {
+        xPos = random.nextInt(200)-100;
+        yPos = random.nextInt(200)-100;
+    }
+    
+    public void locatePlayer(Player p) {
+        if (p.xCoord > xPos) xPos += vel;
+        if (p.xCoord < xPos) xPos -= vel;
+        if (p.yCoord > yPos) yPos += vel;
+        if (p.yCoord < yPos) yPos -= vel;
+    }
+    
+    public void draw() {
+        StdDraw.setPenColor(100,100,100);
+        StdDraw.point(xPos,yPos);
+    }
+    
+    public void update(Player p) {
+        draw();
+        locatePlayer(p);
     }
 }
 
 public class Shooter {
+    public static ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+    public static Player player = new Player();
    
-   public static void initialize() {
+    public static void initialize() {
         StdDraw.enableDoubleBuffering();
         StdDraw.setScale(-100,100);
         StdDraw.setPenRadius(0.05);
-   }
+    }
+    
+    public static void createEnemies() {
+        for (int i=0;i<10;i++) {
+            enemies.add(new Enemy());
+        }
+    }
+    
+    public static void updateEnemies() {
+        for (int i=0;i<enemies.size();i++) {
+            enemies.get(i).update(player);
+        }
+    }
     
     public static void main(String[] args) {
         initialize();
-        Player player = new Player();
         
-        Enemy enemy = new Enemy();
-        System.out.println(enemy.enemies);
+        createEnemies();
         
         while (true) {
            StdDraw.clear(StdDraw.BLACK);
            player.update();
+           
+           updateEnemies();
+           
            StdDraw.show();
         }
     }
