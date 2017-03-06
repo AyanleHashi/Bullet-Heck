1e4c37a00f540fd54ece7cf20a9dc40781cdb781
Ayanle Hashi <ahashi2000@gmail.com>
2017-03-06 13:13:38 -0500

    Update BasicEnemy.java

--- IsaacRemake/BasicEnemy.java
+++ IsaacRemake/BasicEnemy.java
@@ -7,35 +7,40 @@ class BasicEnemy {
     
     public double xPos;
     public double yPos;
-    public double vel = 0.05;
+    public double vel;
     public Player p;
     public Font font;
+    public double[] possibleVels = {0.001,0.002};
     
     public BasicEnemy(Player player) {
         xPos = random.nextInt(200)-100;
         yPos = random.nextInt(200)-100;
         p = player;
         StdDraw.setFont(new Font("Arial",Font.BOLD,25));
+        vel = possibleVels[random.nextInt(possibleVels.length)];
     }
     
     public double getAngle(double x1,double x2,double y1,double y2) {
         double angle = (double) Math.toDegrees(Math.atan2(y2 - y1, x2 - x1));
-        if(angle < 0) angle += 360;
         
         return angle;
     }
     
     public void locatePlayer() {
-        /*if (p.xCoord > xPos) xPos += vel;
-        if (p.xCoord < xPos) xPos -= vel;
-        if (p.yCoord > yPos) yPos += vel;
-        if (p.yCoord < yPos) yPos -= vel;*/
+        double wallSize = 78;
         
-        double angle = getAngle(xPos,p.xCoord,yPos,p.yCoord);
-        double distance = Math.hypot(Math.abs(xPos-p.xCoord),Math.abs(yPos-p.yCoord));
+        double angle = getAngle(xPos,p.xPos,yPos,p.yPos);
+        double distance = Math.hypot(Math.abs(xPos-p.xPos),Math.abs(yPos-p.yPos));
         
-        double deltaX = distance * Math.sin(angle) * (180.0 / Math.PI);
-        xPos += 0.0005 * deltaX;
+        double deltaX = distance * Math.cos(Math.toRadians(angle));
+        double deltaY = distance * Math.sin(Math.toRadians(angle));
+        xPos += vel * deltaX;
+        yPos += vel * deltaY;
+        
+        if (xPos > wallSize) xPos = wallSize;
+        if (xPos < -wallSize) xPos = -wallSize;
+        if (yPos > wallSize) yPos = wallSize;
+        if (yPos < -wallSize) yPos = -wallSize;
     }
     
     public void draw() {
