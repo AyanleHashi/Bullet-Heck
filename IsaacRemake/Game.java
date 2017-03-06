1f9ac6e36ea7c4479a683a61d11cde130f614fd9
Ayanle Hashi <ahashi2000@gmail.com>
2017-03-06 13:13:47 -0500

    Update Game.java

--- IsaacRemake/Game.java
+++ IsaacRemake/Game.java
@@ -4,6 +4,8 @@ import java.awt.Font;
 import java.awt.Color;
 
 public class Game {
+    //TODO: Display health, enemy collision, rooms, bombs
+    
     public static ArrayList<BasicEnemy> basicEnemies = new ArrayList<BasicEnemy>();
     public static Player player = new Player();
     public static Rooms rooms = new Rooms(player);
@@ -13,10 +15,17 @@ public class Game {
         StdDraw.enableDoubleBuffering();
         StdDraw.setScale(-100,100);
         StdDraw.setPenRadius(0.05);
+        StdDraw.setFont(new Font("Arial",Font.BOLD,25));
+    }
+    
+    public static void checkDamage(Player p,BasicEnemy e) {
+        if ((e.xPos-5 < p.xPos && p.xPos < e.xPos+5) && (e.yPos-5 < p.yPos && p.yPos < e.yPos+5)) {
+            p.takeDamage(true);
+        }
     }
     
     public static void createEnemies() {
-        for (int i=0;i<1;i++) {
+        for (int i=0;i<2;i++) {
             basicEnemies.add(new BasicEnemy(player));
         }
     }
@@ -24,6 +33,7 @@ public class Game {
     public static void updateEnemies() {
         for (BasicEnemy basicEnemy: basicEnemies) {
             basicEnemy.update();
+            checkDamage(player,basicEnemy);
         }
     }
     
@@ -35,10 +45,15 @@ public class Game {
         while (true) {
            StdDraw.clear(new Color(100,70,70));
            
-           //updateEnemies();
+           updateEnemies();
            rooms.drawRoom(room[0],room[1]);
            
            player.update();
+           if (player.health == 0.0) {
+               System.out.println("Game over!");
+               System.exit(0);
+           }
+           
            StdDraw.show();
         }
     }
