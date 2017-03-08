ba052115fb6bd9930c7e879e70d8d2e22869151d
Ayanle Hashi <ahashi2000@gmail.com>
2017-03-07 20:28:50 -0500

    Update Rooms.java

--- IsaacRemake/Rooms.java
+++ IsaacRemake/Rooms.java
@@ -6,30 +6,49 @@ class Rooms {
     public Player p;
     public int row = 0;
     public int column = 0;
-    public int[][] rooms = {{0,0,0},
-                            {0,0,0},
-                            {0,0,0}};
+    public int roomID;
+    public int[][] map = {{1,0,0},
+                          {0,0,0},
+                          {0,0,0}};
     
     public Rooms(Player player) {
         p = player;
     }
     
-    public void drawRoom(int row, int column) {
-        StdDraw.setPenColor(255,200,200);
-        StdDraw.filledRectangle(0,90,100,10);
-        StdDraw.filledRectangle(0,-90,100,10);
-        StdDraw.filledRectangle(90,0,10,100);
-        StdDraw.filledRectangle(-90,0,10,100);
-        
-        drawDoors(row,column);
+    public void roomTransition() {
+        if (p.xPos >= 90 && (-5 < p.yPos && p.yPos < 5)) {
+            column += 1;
+            p.xPos = -84;
+            System.out.println(row + " " + column);
+        }
     }
     
-    public void drawDoors(int row, int column) {
-        int room = rooms[row][column];
+    public void drawRoom(int row, int column) {
+        StdDraw.setPenColor(255,200,200);
+        StdDraw.filledRectangle(0,95,100,5);
+        StdDraw.filledRectangle(0,-95,100,5);
+        StdDraw.filledRectangle(95,0,5,100);
+        StdDraw.filledRectangle(-95,0,5,100);
         
-        if (room == 0) {
-            StdDraw.setPenColor(100,70,70);
-            StdDraw.filledRectangle(0,-80,10,3);
+        drawDoors();
+        roomTransition();
+    }
+    
+    public void drawDoors() {
+        int roomID = map[row][column];
+        StdDraw.setPenColor(100,70,70);
+        
+        if (roomID == 0) {
+            StdDraw.filledRectangle(0,-90,10,3);
         }
+        if (roomID == 1) {
+            StdDraw.filledRectangle(0,90,10,3);
+        }
+    }
+    
+    public void update(int r, int c) {
+        row = r; column = c;
+        drawRoom(row,column);
+        roomTransition();
     }
 }
