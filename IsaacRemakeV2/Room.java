769aaf08edc17b02b1fa6bd810a21c09b910fdc4
Ayanle Hashi <ahashi2000@gmail.com>
2017-03-15 18:01:05 -0400

    Update Room.java

--- IsaacRemakeV2/Room.java
+++ IsaacRemakeV2/Room.java
@@ -1,9 +1,53 @@
 import java.util.*;
+import java.awt.*;
 
 public class Room {
     public boolean[] doors = new boolean[4];
+    public Player player;
 
-    public void drawRoom(int x,int y) {
-        StdDraw.filledRectangle(0,90,50,10);
+    public Room(Player p) {
+        player = p;
+    }
+
+    public void drawRoom() {
+        StdDraw.setPenColor(190, 130, 100);
+        StdDraw.filledRectangle(0, 90, 100, 10);
+        StdDraw.filledRectangle(0, -90, 100, 10);
+        StdDraw.filledRectangle(90, 0, 10, 100);
+        StdDraw.filledRectangle(-90, 0, 10, 100);
+    }
+
+    public void drawDoors() {
+        StdDraw.setPenColor(100, 60, 60);
+        if (doors[0]) StdDraw.filledRectangle(0, 90, 11, 11);
+        if (doors[1]) StdDraw.filledRectangle(90, 0, 11, 11);
+        if (doors[2]) StdDraw.filledRectangle(0, -90, 11, 11);
+        if (doors[3]) StdDraw.filledRectangle(-90, 0, 11, 11);
+    }
+
+    public void transition() {
+        if (player.xPos >= 74 && (-5 < player.yPos && player.yPos < 5) && doors[1]) {
+            player.xPos -= 149;
+            player.roomX += 1;
+        }
+        if (player.xPos <= -74 && (-5 < player.yPos && player.yPos < 5) && doors[3]) {
+            player.xPos += 149;
+            player.roomX -= 1;
+        }
+        if (player.yPos >= 74 && (-5 < player.xPos && player.xPos < 5) && doors[0]) {
+            player.yPos -= 149;
+            player.roomY += 1;
+        }
+        if (player.yPos <= -74 && (-5 < player.xPos && player.xPos < 5) && doors[2]) {
+            player.yPos += 149;
+            player.roomX -= 1;
+        }
+    }
+
+    public void update() {
+        drawRoom();
+        drawDoors();
+        transition();
+        System.out.println(doors[0] + " " + doors[1] + " " + doors[2] + " " + doors[3]);
     }
 }
