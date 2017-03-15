694fa6ebb0e12a00a6a9d4858b8577ff67403ede
Ayanle Hashi <ahashi2000@gmail.com>
2017-03-15 09:49:38 -0400

    Create Room.java

---
+++ IsaacRemakeV2/Room.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Room {
+    public boolean[] doors = new boolean[4];
+
+    public void drawRoom(int x,int y) {
+        StdDraw.filledRectangle(0,90,50,10);
+    }
+}
