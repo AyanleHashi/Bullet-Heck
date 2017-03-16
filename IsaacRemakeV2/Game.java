5dcd553acaa5e3c75c0c5a0c61c416bb71c12ce3
Ayanle Hashi <ahashi2000@gmail.com>
2017-03-16 09:20:28 -0400

    Update Game.java

--- IsaacRemakeV2/Game.java
+++ IsaacRemakeV2/Game.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -6,8 +5,8 @@ import java.awt.event.KeyEvent;
 public class Game {
     public static Player player = new Player();
     
-    public static int[][] floorLayout = new int[8][8];
-    public static Room[][] roomLayout = new Room[8][8];
+    public static int[][] floorLayout;
+    public static Room[][] roomLayout;
     
     public static void initializeScreen() {
         StdDraw.enableDoubleBuffering();
@@ -26,7 +25,9 @@ public class Game {
         return count;
     }
 
-    public static void initializeRooms() {
+    public static void initializeRooms(int x, int y) {
+        floorLayout = new int[y][x];
+        roomLayout = new Room[y][x];
         for (int row=0;row<roomLayout.length;row++) {
             for (int col=0;col<roomLayout[row].length;col++) {
                 roomLayout[row][col] = new Room(player);
@@ -34,9 +35,11 @@ public class Game {
         }
     }
     
-    public static void generateFloor(int roomCount) {
+    public static void generateFloor(int roomCount, int x, int y) {
+        initializeRooms(x,y);
         Random random = new Random();
-        int x = 4, y = 4;
+        x = 4;
+        y = 4;
         boolean a;
         
         while (count(1,floorLayout) < roomCount) {
@@ -51,6 +54,7 @@ public class Game {
                 y = 4;
             }
         }
+        generateDoors();
     }
     
     public static void generateDoors() {
@@ -80,7 +84,11 @@ public class Game {
             for (int x=0;x<floorLayout[y].length;x++) {
                 if (floorLayout[y][x] == 1) {
                     StdDraw.setPenColor(100,100,100);
-                    StdDraw.filledRectangle(x*10-90,-y*10+90,4.9,4.9);
+                    if ((y == player.roomX && x == player.roomY)) {
+                        StdDraw.setPenColor(255,255,255);
+                        StdDraw.filledRectangle(x*10-90,-y*10+90,4.9,4.9);
+                    }
+                    else StdDraw.filledRectangle(x*10-90,-y*10+90,4.9,4.9);
                     StdDraw.setPenColor(200, 200, 200);
                     if (roomLayout[y][x].doors[0]) StdDraw.filledRectangle(x * 10 - 90, -y * 10 + 95, 1, 1);
                     if (roomLayout[y][x].doors[1]) StdDraw.filledRectangle(x * 10 - 85, -y * 10 + 90, 1, 1);
@@ -91,26 +99,21 @@ public class Game {
         }
     }
     
-    public static void print(Room[][] arr) {
-        for (int y=0;y<arr.length;y++) {
-            System.out.println(Arrays.toString(arr[y]));
-        }
-    }
-    
     public static void main(String[] args) {
         initializeScreen();
-        initializeRooms();
-        generateFloor(10);
-        generateDoors();
+
+        generateFloor(15,10,10);
         
         while (true) {
             StdDraw.clear(new Color(100,60,60));
-            
-            if (StdDraw.isKeyPressed(KeyEvent.VK_M)) drawMap();
+
             roomLayout[player.roomX][player.roomY].update();
+            if (StdDraw.isKeyPressed(KeyEvent.VK_M)) drawMap();
 
             player.update();
             StdDraw.show();
+
+            if (player.health == 0) System.exit(0);
         }
     }
 }
