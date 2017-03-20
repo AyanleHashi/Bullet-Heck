a566b0a6cf0f19f367a38817c6e14f1f81b55f58
Ayanle Hashi <ahashi2000@gmail.com>
2017-03-20 07:46:23 -0400

    Update Item.java

--- IsaacRemakeV2/Item.java
+++ IsaacRemakeV2/Item.java
@@ -1,7 +1,9 @@
-class Item {
-    public String name;
-    
-    public Item {
-        
-    }
+public interface Item {
+    Player player = new Player();
+    double x = 0;
+    double y = 0;
+
+    void modifier();
+    void draw(double x, double y);
+    void pickup(double x, double y);
 }
